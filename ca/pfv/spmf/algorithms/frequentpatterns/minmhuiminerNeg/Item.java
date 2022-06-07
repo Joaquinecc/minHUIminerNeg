d6752dffaa4ea18218593bda443eaa89339e3f23
joaquin caballero <joaquinecc@gmail.com>
2022-06-06 20:12:41 -0400

    minMHUIMINER-NEG

---
+++ ca/pfv/spmf/algorithms/frequentpatterns/minmhuiminerNeg/Item.java
@@ -0,0 +1,39 @@
+package ca.pfv.spmf.algorithms.frequentpatterns.minmhuiminerNeg;
+
+
+/**
+ * This is an implementation of an Item as used by the mHUIMiner class.
+ *
+ * @author Prashant Barhate, modified by Alex Peng
+ * @see mHUIMinerNegV1
+ */
+
+public class Item {
+
+    private int itemID = 0;
+    private int utility = 0;
+
+    // public Item(int id) {
+    // this.itemID = id;
+    // }
+
+    public Item(int id, int utility) {
+        this.itemID = id;
+        this.utility = utility;
+
+    }
+
+    /**
+     * method to get utility
+     */
+    public int getUtility() {
+        return utility;
+    }
+
+    /**
+     * method to get particular item
+     */
+    public int getItemID() {
+        return itemID;
+    }
+}
